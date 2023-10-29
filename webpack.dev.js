e4f86c381484c37c99e235bcda567c6e601ec4c6
Paula Lippe <71293269+paulajlippe@users.noreply.github.com>
2023-10-29 16:55:16 -0500

    Final touches
    Final touches/spacing

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-// const { GenerateSW } = require('workbox-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -47,6 +46,5 @@ module.exports = {
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         }),
-        // new GenerateSW(),
     ]
 }
